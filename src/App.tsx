4c488519a295fe31b65f998307070736388f2df8
Dmytro Moskvin <d.moskvin@hotmail.com>
2022-07-13 19:14:22 +0300

    Add app behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import type { FunctionComponent } from 'react';
+import type { User } from './behavior/mockServer';
+import type { Api } from 'behavior/api';
+import { type FunctionComponent, useEffect, useState } from 'react';
+import { LoginForm, ProfileForm } from 'components';
 import './App.css';
 
-const App: FunctionComponent = () => {
-  return (
-    <p>App</p>
-  );
+const App: FunctionComponent<{ api: Api }> = ({ api }) => {
+
+  const [user, setUser] = useState<User | null>();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = api.tokenStorage.get();
+
+    if (storedUser) {
+      api.userProvider.getUser(storedUser)
+        .then(u => u && setUser(u))
+        .then(_ => setLoading(false));
+    }
+    else
+      setLoading(false);
+  }, []);
+
+  if (user)
+    return <ProfileForm api={api} isLoading={isLoading} user={user} setUser={setUser} setLoading={setLoading} />
+
+  return <LoginForm api={api} isLoading={isLoading} setUser={setUser} setLoading={setLoading} />
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import createApi from './behavior/api';
 import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const api = createApi();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App api={api} />
   </React.StrictMode>
 );
